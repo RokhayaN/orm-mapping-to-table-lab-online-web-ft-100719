f168945e2a92f0d16d01a2bcd694f88aca704fa6
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-13 20:20:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,8 +31,8 @@ def save
       VALUES ("rokhaya",""3th")
       SQL
       DB[:conn].execute(sql)
- @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
- 
+@id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+
   end
  
  def self.create(name, grade)
