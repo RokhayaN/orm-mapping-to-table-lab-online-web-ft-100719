0078889e0ff4986bcce24675bd18414b641ea3fe
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-13 19:15:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,52 @@
 class Student
+attr_accessor :name, :grade 
+attr_reader :id 
+@@all=[]
+def initialize(name,grade,id=nil)
+  @name = name
+  @grade = grade
+  @id = id
+  @@all << self
+end 
+  
+ def self.create_table
+    sql = <<-SQL
+      CREATE TABLE students (id INTEGER PRIMARY KEY,
+                             name TEXT, 
+                             grade TEXT)
+      SQL
+      DB[:conn].execute(sql)
+  end
+
+def self.drop_table
+  sql = <<-SQL
+      DROP TABLE students
+  SQL
+  DB[:conn].execute(sql)
+end 
+Student.create_table
+student = Student.new("Rokhaya", "26")
+student.save
+
+def self.save 
+  sql = <<-SQL 
+      
+      SQL
+      DB[:conn].execute(sql)
+end    
+
+ def self.create(name, grade)
+    student = Student.new(name,grade)
+    student.save
+    student
+  end
+end
+
+
+
+
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
-end
+
